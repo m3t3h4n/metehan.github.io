976251921cfd3cf3f3cfb9c06fff777eac4e95af
Metehan <47110953+m3t3h4n@users.noreply.github.com>
2019-04-24 19:42:30 +0300

    website page chnage
    website page chnage

--- _js/src/push-state.js
+++ _js/src/push-state.js
@@ -1,25 +1,4 @@
-// # src / push-state.js
-// Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// ## Overview
-// This file sets up the hy-push-state component,
-// which is responsible for dynimically changing the content when users click on links.
-// However, the component only handles changing the content.
-// Animating it, responding to errors and showing loading spinners is still up to us,
-// which is the purpose of this file.
 
 // ## Includes
 // First, we patch the environment with some ES6+ functions we intend to use.
