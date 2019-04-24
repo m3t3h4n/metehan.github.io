daccd28e68d954f334effb39f338e3dae11f1112
Metehan <47110953+m3t3h4n@users.noreply.github.com>
2019-04-24 19:39:30 +0300

    web site data changes

--- _js/src/drawer.js
+++ _js/src/drawer.js
@@ -1,23 +1,4 @@
-// # src / drawer.js
-// Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/* eslint-disable no-mixed-operators */
-
-// ## Includes
-// First, we patch the environment with some ES6+ functions we intend to use.
 import "core-js/fn/function/bind";
 
 import ResizeObserver from "resize-observer-polyfill";
